42b11d7e77e840ae31bb648067f343ef0597cc98
neetesh <neeteshlodhi9555@email.com>
2026-05-15 21:35:47 +0530

    two sum 2

---
+++ practice/Two.java
@@ -0,0 +1,23 @@
+public class Two {
+          public int[] twSum(int[] numbers, int target) {
+                    int n = numbers.length; // T.C-O(n)
+                    int sum = 0;
+                    int left = 0;
+                    int right = n - 1;
+
+                    for (int i = 0; i < n; i++) {
+                              sum = numbers[left] + numbers[right];
+
+                              if (sum == target)
+                                        return new int[] { left + 1, right + 1 };
+
+                              if (sum < target) {
+                                        left++;
+                              } else if (sum > target) {
+                                        right--;
+                              }
+                    }
+
+                    return new int[] { -1, -1 };
+          } 
+}
