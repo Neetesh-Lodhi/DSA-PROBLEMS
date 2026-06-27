2e3036159f8d7b2ca82c2cef4394262c0ff7a827
neetesh <neeteshlodhi9555@email.com>
2026-06-27 12:01:50 +0530

    reverse linkedlist

---
+++ practice/LinkedListRev.java
@@ -0,0 +1,14 @@
+public class LinkedListRev {
+          public ListNode reverseList(ListNode head) {
+                    ListNode curr = head;
+                    ListNode prev = null;
+                    while (curr != null) {
+                              ListNode temp = curr.next;
+                              curr.next = prev;
+                              prev = curr;
+                              curr = temp;
+                    }
+
+                    return prev;
+          }   
+}
